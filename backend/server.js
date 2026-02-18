6edf9a3523ef5ca61a7224863fe46b80b6b68e8d
TamilarasiG3 <your-email@example.com>
2026-02-18 23:15:27 +0530

    backend ready

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
 
 const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
